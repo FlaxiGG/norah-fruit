f9c320dd1ed9d4e07dba1a8294b9b41ca9847561
FlaxiGG <mickza050@gmail.com>
2025-01-02 15:25:15 +0700

    Install Node

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+// npm install mysql2 [Done]
+// npm install express [Done]
+// npm install cors [Done]
+// npm install uuid [Done]
+// npm install axios (Global Install) [Done]
+// npm install sweetalert2
+// npm install jsonwebtoken [Done]
